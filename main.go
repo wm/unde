8a14e7459b201a3755e8aed93bfaa6ce7f32ae99
Will Mernagh <wmernagh@gmail.com>
2015-01-08 21:31:37 -0500

    Statements part 3 (conditionals)

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ type Expression interface {
 	String() string
 	Reducible() bool
 	Reduce(environment map[string]Expression) Expression
+	Equal(Expression) bool
 }
 
 type Boolean struct {
@@ -27,6 +28,11 @@ func (b Boolean) String() string {
 	return strconv.FormatBool(b.Value)
 }
 
+func (b Boolean) Equal(other Expression) bool {
+	otherBoolean, ok := other.(Boolean)
+	return ok && b.Value == otherBoolean.Value
+}
+
 type LessThan struct {
 	Left  Expression
 	Right Expression
@@ -34,6 +40,11 @@ type LessThan struct {
 
 func (lt LessThan) String() string {
 	return ""
+}
+
+func (lt LessThan) Equal(other Expression) bool {
+	otherLt, ok := other.(LessThan)
+	return ok && lt.Left == otherLt.Left && lt.Right == otherLt.Right
 }
 
 func (lt LessThan) Reducible() bool {
@@ -54,6 +65,11 @@ type Number struct {
 	Value int
 }
 
+func (n Number) Equal(other Expression) bool {
+	otherNum, ok := other.(Number)
+	return ok && n.Value == otherNum.Value
+}
+
 func (n Number) Reducible() bool {
 	return false
 }
@@ -69,6 +85,11 @@ func (n Number) String() string {
 type Add struct {
 	Left  Expression
 	Right Expression
+}
+
+func (a Add) Equal(other Expression) bool {
+	otherAdd, ok := other.(Add)
+	return ok && a.Left == otherAdd.Left && a.Right == otherAdd.Right
 }
 
 func (a Add) Reducible() bool {
@@ -94,6 +115,11 @@ type Multiply struct {
 	Right Expression
 }
 
+func (a Multiply) Equal(other Expression) bool {
+	otherMultiply, ok := other.(Multiply)
+	return ok && a.Left == otherMultiply.Left && a.Right == otherMultiply.Right
+}
+
 func (a Multiply) Reducible() bool {
 	return true
 }
@@ -114,6 +140,11 @@ func (m Multiply) String() string {
 
 type Variable struct {
 	Name string
+}
+
+func (v Variable) Equal(other Expression) bool {
+	otherVariable, ok := other.(Variable)
+	return ok && v.Name == otherVariable.Name
 }
 
 func (v Variable) String() string {
@@ -168,20 +199,21 @@ func (a Assign) Reducible() bool {
 }
 
 func (a Assign) Equal(other Statement) bool {
-	return true
+	otherAssign, ok := other.(Assign)
+	return ok && a.Name == otherAssign.Name && a.Expression == otherAssign.Expression
 }
 
 func (a Assign) Reduce(environment map[string]Expression) (Statement, map[string]Expression) {
 	if a.Expression.Reducible() {
 		return Assign{a.Name, a.Expression.Reduce(environment)}, environment
 	} else {
-		newEnv := copyMap(environment)
+		newEnv := copyEnvironment(environment)
 		newEnv[a.Name] = a.Expression
 		return DoNothing{}, newEnv
 	}
 }
 
-func copyMap(src map[string]Expression) map[string]Expression {
+func copyEnvironment(src map[string]Expression) map[string]Expression {
 	cpy := make(map[string]Expression)
 
 	for k, v := range src {
@@ -189,6 +221,41 @@ func copyMap(src map[string]Expression) map[string]Expression {
 	}
 
 	return cpy
+}
+
+type If struct {
+	Condition   Expression
+	Consequence Statement
+	Alternative Statement
+}
+
+func (i If) String() string {
+	return fmt.Sprintf("if %s { %s } else { %s }", i.Condition, i.Consequence, i.Alternative)
+}
+
+func (i If) Equal(other Statement) bool {
+	otherIf, ok := other.(If)
+	return ok && i.Condition == otherIf.Condition && i.Consequence == otherIf.Consequence && i.Alternative == otherIf.Alternative
+}
+
+func (i If) Reducible() bool {
+	return true
+}
+
+func (i If) Reduce(environment map[string]Expression) (Statement, map[string]Expression) {
+	switch {
+	case i.Condition.Reducible():
+		return If{i.Condition.Reduce(environment), i.Consequence, i.Alternative}, environment
+	case i.Condition.Equal(Boolean{true}):
+		return i.Consequence, environment
+	case i.Condition.Equal(Boolean{false}):
+		return i.Alternative, environment
+	default:
+		// Execution should never reach here but Golang complains.
+		// I want to be explicit for true and false so I will keep both and
+		// this.
+		return i.Alternative, environment
+	}
 }
 
 type Machine struct {
@@ -212,6 +279,13 @@ func main() {
 	machine := Machine{
 		Assign{"x", Add{Variable{"x"}, Number{1}}},
 		map[string]Expression{"x": Number{2}},
+	}
+	machine.Run()
+
+	fmt.Println("\n")
+	machine = Machine{
+		If{Variable{"x"}, Assign{"y", Number{1}}, DoNothing{}},
+		map[string]Expression{"x": Boolean{false}},
 	}
 	machine.Run()
 }
